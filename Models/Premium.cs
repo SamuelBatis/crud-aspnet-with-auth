4347109de8ce83f3bad89aad73403a12b4aac8d5
Samuel <samuel.batistela12@gmail.com>
2024-03-24 21:50:33 -0300

    feat: add entities

---
+++ Models/Premium.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace crud_aspnet_with_auth.Models
+{
+    public class Premium
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o titulo do Premium")]
+        [StringLength(80, ErrorMessage = "O Titulo deve conter até 80 caracteres")]
+        [MinLength(5, ErrorMessage = "O Titulo deve conter pelo menos 5 caracteres")]
+        [DisplayName("Titulo")]
+        public string Title { get; set; }
+
+        [DataType(DataType.DateTime)]
+        // [GreaterThanToday]
+        [DisplayName("Inicio")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Termino")]
+        public DateTime EndDate { get; set; }
+
+
+        [DisplayName("Aluno")]
+        [Required(ErrorMessage = "Aluno invalido")]
+        public DateTime StudentId { get; set; }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace crud_aspnet_with_auth.Models
+{
+    public class Student
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o nome")]
+        [StringLength(80, ErrorMessage = "O nome deve conter até 80 caracteres")]
+        [MinLength(5, ErrorMessage = "O nome deve conter pelo menos 5 caracters")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Informe o E-mail")]
+        [EmailAddress(ErrorMessage ="Email invalido")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        public List<Premium> Students { get; set; } = new();
+    }
+}
